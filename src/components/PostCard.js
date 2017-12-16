187a546559331404542904cf4bb9efb0b68abdbf
sergemin <minin.serhii@gmail.com>
2017-12-16 08:17:15 +0200

    merge to master

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -8,7 +8,7 @@ function PostCard(props) {
         <div className="post-item">
             <div className="post-item__inner">
                 <h3 className="post-item__title">{props.user.login}</h3>
-                <img className="post-item__img" src={props.user.avatar_url} alt="this.props.user" />
+                <img className="post-item__img" src={props.user.avatar_url} alt={props.user.login} />
             </div>
         </div>
     )
