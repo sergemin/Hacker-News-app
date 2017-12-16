a5bef54a8e390664762735b7e8e4bb8692adcbbf
sergemin <minin.serhii@gmail.com>
2017-12-16 09:16:36 +0200

    routing starts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import PostPage from './components/PostPage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App}/>
+            <Route path="/posts/:postID" component={PostPage}/>
+        </Switch>
+    </Router>, document.getElementById('root'));
 registerServiceWorker();
