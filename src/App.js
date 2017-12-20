fc970c663ee7def4fc3d0b1a4d6d4fb956f3729a
Serhii <minin.serhii@gmail.com>
2017-12-20 06:11:13 +0200

    troubles with logic

--- src/App.js
+++ src/App.js
@@ -9,53 +9,39 @@ export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts: []
-        }
+            posts: [],
+            pagination: [1, 2, 3]
+        };
+        this.countOfElementsInPag = 10;
     }
     getPosts() {
-        let userID = this.props.match.params.userID ? this.props.match.params.userID : 1;
-        fetchJSON(`https://jsonplaceholder.typicode.com/posts?userId=${userID}`)
+
+    }
+    componentDidMount() {
+        fetchJSON(`https://jsonplaceholder.typicode.com/posts`)
             .then(response => {
                 this.setState({
                     posts: response
-                }, () => console.log(this.state.posts))
+                })
             })
             .catch(error => {
                 console.log('request failed', error)
             });
     }
-    componentDidMount() {
-        this.getPosts();
-    }
     render() {
+        let countOfPagesPag = Math.ceil(this.state.posts.length/this.countOfElementsInPag);
+        console.log(countOfPagesPag);
         return (
             <div className="App">
                 <h1 className="section-title">My React app with fake REST API</h1>
                 <div className="container">
                     <ul className="block-list">
-                        {this.state.posts.map((item) => (
+                        {this.state.posts.map( item => (
                             <li key = {item.id}
                                 className="post-list__item">
                                 <PostCard post = {item} />
                             </li> 
                         ))}
-                    </ul>
-                    <ul className="pagination">
-                        <li className="pagination__item">
-                            <Link to="/"
-                                  className='pagination__link'
-                                  onClick={this.getPosts.bind(this)}>1</Link>
-                        </li>
-                        <li className="pagination__item">
-                            <Link to="/2"
-                                  className='pagination__link'
-                                  onClick={this.getPosts.bind(this)}>2</Link>
-                        </li>
-                        <li className="pagination__item">
-                            <Link to="/3"
-                                  className='pagination__link'
-                                  onClick={this.getPosts.bind(this)}>3</Link>
-                        </li>
                     </ul>
                 </div>
             </div>
