a5441621bb91800d801cd9951529ed5e0cfd784d
Serhii Minin <sergey.minin@brainberry.ua>
2018-02-15 18:09:56 +0200

    add improvements for list of posts

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,20 +6,13 @@ const Header = () => (
     <header className="header">
         <div className="container">
           <h1 className="section-title">Hackernews App</h1>
-            <div className="header-block">
-                <div className="header__description">
-                    <p className="header__description-text">
-                        Build apps with us!
-                    </p>
-                </div>
-                <nav className="header-nav">
-                    <ul className="header__menu header-menu">
-                        <li className="header-menu__item">
-                            <Link className='header-menu__link link-app' to='/'>Home page</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+          <nav className="header-nav">
+            <ul className="header__menu header-menu">
+              <li className="header-menu__item">
+                <Link className='header-menu__link link-app' to='/'>Home page</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
     </header>
 );
--- src/components/post-card/index.js
+++ src/components/post-card/index.js
@@ -2,33 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+import { Card, Badge, Avatar, Icon } from 'antd';
 import './styles.scss';
 
 const PostCard = ({ post }) => (
   <div className='post-card'>
-    <div className='post-card__inner'>
-      <div className="posts-card__title">
-        {post.title}
+    <Card
+      actions={[<Link to={`/posts/${post.id}`}> Learn More</Link>]}>
+      <Card.Meta
+        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+        title={<h2 className="posts-card__title">{post.title}</h2>}
+        description={`${post.by}, ${moment(post.time).format('MM/DD/YYYY')}`}
+      />
+    </Card>
+    <div className='post-card'>
+      <div className='post-card__inner'>
+        <div className="posts-card__title">
+          {post.title}
+        </div>
+        <div className="posts-card__author">
+          {post.by}
+        </div>
+        <div className="posts-card__score">
+          {post.score}
+        </div>
+        <div className="posts-card__date">
+          {moment(post.time).format('MM DD YYYY')}
+        </div>
+        <div className="posts-card__comments">
+          {post.kids ? post.kids.length : 0}
+        </div>
       </div>
-      <div className="posts-card__author">
-        {post.by}
-      </div>
-      <div className="posts-card__score">
-        {post.score}
-      </div>
-      <div className="posts-card__date">
-        {moment(post.time).format('MM DD YYYY')}
-      </div>
-      <div className="posts-card__comments">
-        {post.kids ? post.kids.length : 0}
-      </div>
-    </div>
-    <div>
       <div>
-        <Link to={`/posts/${post.id}`}> Learn More</Link>
+        <div>
+          <Link to={`/posts/${post.id}`}> Learn More</Link>
+        </div>
       </div>
     </div>
   </div>
+
 );
 
 PostCard.propTypes = {
